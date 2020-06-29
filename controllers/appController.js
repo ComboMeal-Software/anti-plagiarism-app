c61c2c1d09d9026c3838df5659e01f006abce0a8
Thomas Miles <Dmilya2000@gmail.com>
2020-06-29 10:51:33 +0300

    routing

---
+++ controllers/appController.js
@@ -0,0 +1,8 @@
+
+exports.homePage = (req, res, next) => {
+    res.render('index', { title: 'Anti-Plagiarism System' });
+};
+
+exports.homePageResult = (req, res, next) => {
+    res.render('index', { title: 'Anti-Plagiarism System' });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const appController = require('../controllers/appController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', appController.homePage);
+router.post('/', appController.homePageResult);
 
 module.exports = router;
