0f3c77a0a7db0093b3660a03971f1a0d5c12fd16
DonTsipa <54895675+DonTsipa@users.noreply.github.com>
2020-07-05 01:11:15 +0400

    Some major fixes

--- routes/compare.route.js
+++ routes/compare.route.js
@@ -1,39 +1,43 @@
 const express = require('express');
 const router = express.Router();
-const { normalizeText } = require('normalize-text');
 const Tokenizr = require('tokenizr');
 const c_rules = require('../tokenRules/c-tokenizer')
-const lexer = new Tokenizr()
+let lexer = new Tokenizr()
 router.post('/', (req, res) => {
   let {
     first,
     second
   } = req.body;
   try {
-    first = normalizeText([req.body.first]);
-    second = normalizeText([req.body.second]);
-    
-    c_rules.forEach(rule => {
-      lexer.rule(rule.regex, (ctx, match) => {
-        ctx.accept(rule.type)
+
+    c_rules.forEach((c_rule) => {
+      lexer.rule(c_rule.regex, (ctx, match) => {
+        if (c_rule.type.length) {
+          ctx.accept(c_rule.type)
+        } else { 
+          ctx.ignore()
+        }
       })
-      lexer.rule(/\/\/[^\r\n]*\r?\n/, (ctx, match) => {
-        ctx.ignore()
-      })
-      lexer.rule(/[ \t\r\n]+/, (ctx, match) => {
-        ctx.ignore()
-      })
-    });
-    lexer.input(first);
+   })
+    lexer.rule(/[a-zA-Z_][a-zA-Z0-9_]*/, (ctx, match) => {
+      ctx.accept("id")
+    })
+    lexer.input(first.toLowerCase());
     lexer.debug(false);
+    let firstToken = ''
     lexer.tokens().forEach((token) => {
-      console.log(token.toString())
+      firstToken += token.type.toString()
+      console.log(token.type.toString());
     });
-    console.log('Second file');
-    lexer.input(second);
+    console.log(firstToken)
+    lexer.input(second.toLowerCase());
     lexer.debug(false);
+    let secondToken = ''
     lexer.tokens().forEach((token) => {
+      secondToken += token.type.toString()
+      console.log(token.type.toString());
     });
+    console.log(secondToken)
   } catch (e) {
     res.status(500);
     console.log(e);
