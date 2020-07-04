5c23d141ba6a20ff7781e361ccb83467fd58452d
DonTsipa <54895675+DonTsipa@users.noreply.github.com>
2020-07-04 21:36:05 +0400

    normolizing init

--- routes/compare.route.js
+++ routes/compare.route.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const { normalizeText } = require('normalize-text');
 router.post('/', (req, res) => {
-  const {
+  let {
     first,
     second
   } = req.body;
-  console.log(req.body);
+  try {
+    first = normalizeText([req.body.first]);
+    second = normalizeText([req.body.second]);
+    console.log(first, second);
+  } catch (e) {
+    res.status(500);
+    console.log(e);
+  }
 })
 
 module.exports = router;
