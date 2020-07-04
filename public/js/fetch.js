752cb8a20324b4570620f6f8c79cc2878dda9c10
DonTsipa <54895675+DonTsipa@users.noreply.github.com>
2020-07-04 19:33:07 +0400

    The application/json header is added

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -2,15 +2,18 @@ const form = document.querySelector('.main')
 const first = document.querySelectorAll('.form_textarea')[0]
 const second = document.querySelectorAll('.form_textarea')[1]
 let method = 'POST'
+let headers = {}
 form.addEventListener('submit', async (event) => {
   event.preventDefault()
   const body = JSON.stringify({
     first: first.value,
     second: second.value,
   })
+  headers['Content-Type'] = 'application/json'
   const response = await fetch('http://localhost:3000/compare', {
     method,
     body,
+    headers
   })
   const status = response.status;
 
