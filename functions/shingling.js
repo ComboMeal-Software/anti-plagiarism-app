546238d2f2af0008c36ea6c45ecdbfd9495f7fe4
Thomas Miles <Dmilya2000@gmail.com>
2020-07-06 18:17:04 +0300

    result calculation fix

--- functions/shingling.js
+++ functions/shingling.js
@@ -1,5 +1,7 @@
 const token = require('./token');
 const CRC32 = require('crc-32');
+const intersection = require('array-intersection');
+const union = require('array-union');
 
 exports.getShingle = (first, second, N) => {
     let {
@@ -24,16 +26,18 @@ exports.getShingle = (first, second, N) => {
       secondHash.push(CRC32.str(item));
     })
     
-    let similarHash = [];
-    firstHash.forEach(function(item1, i1, arr1) {
-      secondHash.forEach(function(item2, i2, arr2){
-        if ((item1 == item2) && (similarHash.indexOf(item1) == -1)){
-          similarHash.push(item1);
-        }
-      })
-    })
+    // let similarHash = [];
+    // firstHash.forEach(function(item1, i1, arr1) {
+    //   secondHash.forEach(function(item2, i2, arr2){
+    //     if ((item1 == item2) && (similarHash.indexOf(item1) == -1)){
+    //       similarHash.push(item1);
+    //     }
+    //   })
+    // })
 
-    const plagiarized = (similarHash.length/(firstHash.length + secondHash.length - 2 * similarHash.length)) * 100;
+    // const plagiarized = (similarHash.length/(firstHash.length + secondHash.length - 2 * similarHash.length)) * 100;
+
+    const plagiarized = intersection(firstHash, secondHash).length / union(firstHash, secondHash).length * 100;
     const result = {
       method: 'shingling',
       value: plagiarized,
--- routes/compare.route.js
+++ routes/compare.route.js
@@ -24,7 +24,7 @@ router.post('/shingling', (req, res) => {
     second,
   } = req.body;
   try {
-    const result = shingle.getShingle(first, second, 6);
+    const result = shingle.getShingle(first, second, 4);
     res.status(200).json({
       result,
     });
