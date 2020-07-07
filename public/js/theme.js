3acb9a73956e50aeeaa9b6e7a05104a03a3ea0a9
DonTsipa <54895675+DonTsipa@users.noreply.github.com>
2020-07-08 00:18:12 +0400

    GACHI TIME

--- public/js/theme.js
+++ public/js/theme.js
@@ -13,17 +13,22 @@ theme_button.addEventListener('click', (event) => {
     gachiPointes++;
   }
   if (gachiPointes > 10) {
-    theme = 'dark';
+    const headerText = document.querySelector('.header').querySelector('h1');
     body.style = ' background: url(../img/guchiLoad.gif) no-repeat; background-size: cover;'
-    console.log('GACHI TIME')
+    theme_button.innerText = 'GACHI TIME';
+    theme_button.style = ('background-color: #a4508b; background-image: linear-gradient(326deg, #a4508b 0%, #5f0a87 74%);')
+    headerText.innerText = 'GACHI TIME'
+    headerText.style = 'background: -webkit-linear-gradient(315deg, #f6f0c4 0%, #d99ec9 74%); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'
     setTimeout(() => {
       gachiPointes = 0;
       theme = 'light'
-      theme_button.innerText = "Светлая тема";
+      theme_button.innerText = 'Светлая тема';
       textarea.forEach((area) => { area.classList.add('textarea_light') })
-      theme_button.style = "background-color: #f9f9f9; color:#40434c;"
+      theme_button.style = 'background-color: #f9f9f9; color:#40434c;'
       header.style = 'color:#fff'
       body.style = 'background-color: #131418'
+      headerText.innerText = 'Прототип системы проверки исходного кода программ на плагиат'
+      headerText.style = ''
     }, 6000)
   } else {
     if (theme === 'light') {
