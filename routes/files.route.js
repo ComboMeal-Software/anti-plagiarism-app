2db256f13734d83b72b7a0edcc91b22a40414fb3
Thomas Miles <Dmilya2000@gmail.com>
2020-07-09 23:34:34 +0300

    file uploading

--- routes/files.route.js
+++ routes/files.route.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 
-var storage = multer.diskStorage({
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+const token = require('../functions/token');
+
+const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '../public/uploads')
+        cb(null, 'public/uploads')
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
@@ -13,12 +17,26 @@ var storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.post('/leven', upload.any('files', 100), (req, res) => {
-    req.files.forEach(function(item) {
-        
-    })
+router.post('/leven', upload.any('files'), (req, res) => {
+    try {
+        req.files.forEach((item) => {
+            let filePath = path.join(__dirname, '../public/uploads/', item.filename)
+            fs.readFile(filePath, 'utf8', function(err, contents) {
+                console.log(filePath);
+                let {
+                    firstToken,
+                    secondToken,
+                  } = token.getTokens(contents, ' ');
+                console.log(firstToken);
+            });
+        });
+    } catch(e) {
+        console.log(e);
+    };
 });
 
-router.post('/shingling', upload.any('files', 100), (req, res) => {
+router.post('/shingling', upload.array('files'), (req, res) => {
 
 });
+
+module.exports = router;
