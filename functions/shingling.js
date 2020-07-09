de2aaac15a45ab852770627697ecbc6188fe1f6b
Thomas Miles <Dmilya2000@gmail.com>
2020-07-09 23:35:11 +0300

    token rewrite for dynamic input length

--- functions/shingling.js
+++ functions/shingling.js
@@ -4,13 +4,10 @@ const intersection = require('array-intersection');
 const union = require('array-union');
 
 exports.getShingle = (first, second, N) => {
-    let {
-      firstToken,
-      secondToken,
-    } = token.getTokens(first, second);
+    const tokens = token.getTokens(first, second);
     let firstShingles = [];
-    for (let i = 0; i + N <= firstToken.length; i++) {
-      firstShingles.push(firstToken.substr(i, N));
+    for (let i = 0; i + N <= tokens[0].length; i++) {
+      firstShingles.push(tokens[0].substr(i, N));
     }
     let firstHash = [];
     firstShingles.forEach(function(item, i, arr) {
@@ -18,25 +15,14 @@ exports.getShingle = (first, second, N) => {
     })
 
     let secondShingles = [];
-    for (let i = 0; i + N <= secondToken.length; i++) {
-      secondShingles.push(secondToken.substr(i, N));
+    for (let i = 0; i + N <= tokens[1].length; i++) {
+      secondShingles.push(tokens[1].substr(i, N));
     }
     let secondHash = [];
     secondShingles.forEach(function(item, i, arr) {
       secondHash.push(CRC32.str(item));
     })
     
-    // let similarHash = [];
-    // firstHash.forEach(function(item1, i1, arr1) {
-    //   secondHash.forEach(function(item2, i2, arr2){
-    //     if ((item1 == item2) && (similarHash.indexOf(item1) == -1)){
-    //       similarHash.push(item1);
-    //     }
-    //   })
-    // })
-
-    // const plagiarized = (similarHash.length/(firstHash.length + secondHash.length - 2 * similarHash.length)) * 100;
-
     const plagiarized = intersection(firstHash, secondHash).length / union(firstHash, secondHash).length * 100;
     const result = {
       method: 'shingling',
--- functions/token.js
+++ functions/token.js
@@ -2,7 +2,7 @@ const Tokenizr = require('tokenizr');
 const c_rules = require('../tokenRules/c-tokenizer')
 
 let lexer = new Tokenizr()
-exports.getTokens = (first, second) => {
+exports.getTokens = (...strings) => {
     c_rules.forEach((c_rule) => {
         lexer.rule(c_rule.regex, (ctx, match) => {
         if (c_rule.type.length) {
@@ -12,17 +12,14 @@ exports.getTokens = (first, second) => {
         }
         });
     });
-    lexer.input(first.toLowerCase());
-    lexer.debug(false);
-    let firstToken = '';
-    lexer.tokens().forEach((token) => {
-        firstToken += token.type.toString();
+    const tokens = strings.map((string) => {
+        lexer.input(string.toLowerCase());
+        lexer.debug(false);
+        let stringToken = '';
+        lexer.tokens().forEach((token) => {
+            stringToken += token.type.toString();
+        });
+        return stringToken;
     });
-    lexer.input(second.toLowerCase());
-    lexer.debug(false);
-    let secondToken = '';
-    lexer.tokens().forEach((token) => {
-        secondToken += token.type.toString();
-    });
-    return { firstToken, secondToken }
+    return tokens;
 };
