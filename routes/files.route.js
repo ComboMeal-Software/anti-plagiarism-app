dcbf3078a66ca75c3334f39befea07fa3e69c881
DonTsipa <54895675+DonTsipa@users.noreply.github.com>
2020-07-10 16:05:15 +0400

    rewritten using objects and creating folders

--- routes/files.route.js
+++ routes/files.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const multer = require('multer');
-const fs = require('fs').promises;
+const fs = require('fs-extra').promises;
 const util = require('util');
 const path = require('path');
 const leven = require('../functions/leven');
@@ -13,9 +13,21 @@ const getLevenWait = util.promisify(leven.getLeven);
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         if (file.fieldname === 'first-folder') {
-            cb(null, 'public/uploads/first-folder');
+            fs.mkdirp('public/uploads/first-folder', (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    cb(null, 'public/uploads/first-folder');
+                }
+            })
         } else {
-            cb(null, 'public/uploads/second-folder');
+            fs.mkdirp('public/uploads/second-folder', (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    cb(null, 'public/uploads/second-folder');
+                }
+            });
         };
     },
     filename: (req, file, cb) => {
@@ -38,33 +50,17 @@ router.post('/leven', upload.fields([{ name: 'first-folder', maxCount: 100 }, { 
         console.log('files uploaded');
         const sourcesFirst = new Array();
         let filePath = '';
-        // await req.files['first-folder'].forEach(async (file) => {
-        //     console.log('1');
-        //     filePath = path.join(__dirname, '../', file.path);
-        //     sourcesFirst.push(await fs.readFile(filePath, 'utf8'));
-        //     console.log('2');
-        // });
         for (const file of req.files['first-folder']) {
             filePath = path.join(__dirname, '../', file.path);
+            fs.readFile(filePath, 'utf8')
             sourcesFirst.push([file.filename, await fs.readFile(filePath, 'utf8')]);
         }
         const sourcesSecond = new Array();
-        // await req.files['second-folder'].forEach(async (file) => {
-        //     console.log('3');
-        //     filePath = path.join(__dirname, '../', file.path);
-        //     sourcesSecond.push(await fs.readFile(filePath, 'utf8'));
-        //     console.log('4');
-        // });
         for (const file of req.files['second-folder']) {
             filePath = path.join(__dirname, '../', file.path);
             sourcesSecond.push([file.filename, await fs.readFile(filePath, 'utf8')]);
         }
         let results = new Array();
-        // sourcesFirst.forEach((sourceFirst) => {
-        //     sourcesSecond.forEach((sourceSecond) => {
-        //         results.push([sourceFirst[0], sourceSecond[0], leven.getLeven(sourceFirst[1], sourceSecond[1])]);
-        //     });
-        // });
         for (const sourceFirst of sourcesFirst) {
             for (const sourceSecond of sourcesSecond) {
                 results.push([sourceFirst[0], sourceSecond[0], leven.getLeven(sourceFirst[1], sourceSecond[1])]);
@@ -72,7 +68,7 @@ router.post('/leven', upload.fields([{ name: 'first-folder', maxCount: 100 }, { 
         }
         console.table(results);
         res.send('end');
-    } catch(e) {
+    } catch (e) {
         res.status(500);
         console.log(e);
     };
@@ -85,22 +81,28 @@ router.post('/shingling', upload.fields([{ name: 'first-folder', maxCount: 100 }
         let filePath = '';
         for (const file of req.files['first-folder']) {
             filePath = path.join(__dirname, '../', file.path);
-            sourcesFirst.push([file.filename, await fs.readFile(filePath, 'utf8')]);
+            sourcesFirst.push({
+                name: file.filename,
+                source: await fs.readFile(filePath, 'utf8')
+            });
         }
         const sourcesSecond = new Array();
         for (const file of req.files['second-folder']) {
             filePath = path.join(__dirname, '../', file.path);
-            sourcesSecond.push([file.filename, await fs.readFile(filePath, 'utf8')]);
+            sourcesSecond.push({
+                name: file.filename,
+                source: await fs.readFile(filePath, 'utf8')
+            });
         }
-        let results = new Array();
-        for (const sourceFirst of sourcesFirst) {
-            for (const sourceSecond of sourcesSecond) {
-                results.push([sourceFirst[0], sourceSecond[0], shingle.getShingle(sourceFirst[1], sourceSecond[1])]);
-            }
-        }
+        let results = new Array()
+        sourceFirst.forEach((firstFolderFiles) => {
+            sourceSecond.forEach((secondFolderFiles) => {
+                results.push([firstFolderFiles.name, secondFolderFiles.name, shingle.getShingle(firstFolderFiles.source, secondFolderFiles.source)]);
+            })
+        })
         console.table(results);
         res.send('end');
-    } catch(e) {
+    } catch (e) {
         res.status(500);
         console.log(e);
     };
